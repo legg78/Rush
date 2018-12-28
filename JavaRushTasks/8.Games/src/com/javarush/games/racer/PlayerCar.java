6edb094336a3b4e813be4a22e3555bc2555868da
Tkhor <tkhor@bpcbt.com>
2018-12-28 21:29:45 +0300

    Game RacerGame 9/28

--- JavaRushTasks/8.Games/src/com/javarush/games/racer/PlayerCar.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/racer/PlayerCar.java
@@ -3,7 +3,22 @@ package com.javarush.games.racer;
 public class PlayerCar extends  GameObject {
     private static int playerCarHeight = ShapeMatrix.PLAYER.length;
     public int speed = 1;
+    private Direction direction;
 
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public void move() {
+        if (direction.equals(Direction.LEFT))
+            this.x--;
+        if (direction.equals(Direction.RIGHT))
+            this.x++;
+    }
     public PlayerCar() {
         super(RacerGame.WIDTH / 2 + 2, RacerGame.HEIGHT - playerCarHeight - 1, ShapeMatrix.PLAYER);
     }
--- JavaRushTasks/8.Games/src/com/javarush/games/racer/RacerGame.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -61,6 +61,14 @@ public class RacerGame extends Game {
     }
     private void moveAll() {
         roadMarking.move(player.speed);
+        player.move();
     }
 
+    @Override
+    public void onKeyPress(Key key) {
+        if (key.equals(Key.RIGHT))
+            player.setDirection(Direction.RIGHT);
+        if (key.equals(Key.LEFT))
+            player.setDirection(Direction.LEFT);
+    }
 }
