cb6f14fc3a0c205135768b44dce00245a42b41de
tav <tula78@mail.ru>
2018-12-29 01:10:21 +0300

    Game RacerGame 10/28

--- JavaRushTasks/8.Games/src/com/javarush/games/racer/PlayerCar.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/racer/PlayerCar.java
@@ -1,5 +1,7 @@
 package com.javarush.games.racer;
 
+import com.javarush.games.racer.road.RoadManager;
+
 public class PlayerCar extends  GameObject {
     private static int playerCarHeight = ShapeMatrix.PLAYER.length;
     public int speed = 1;
@@ -14,12 +16,27 @@ public class PlayerCar extends  GameObject {
     }
 
     public void move() {
+
+        if (this.x < RoadManager.LEFT_BORDER) {
+            this.x = RoadManager.LEFT_BORDER;
+            return;
+        }
+
+        if (this.x > (RoadManager.RIGHT_BORDER - width)) {
+            this.x = RoadManager.RIGHT_BORDER - width;
+            return;
+        }
+
         if (direction.equals(Direction.LEFT))
             this.x--;
+
         if (direction.equals(Direction.RIGHT))
             this.x++;
+
+
+
     }
     public PlayerCar() {
-        super(RacerGame.WIDTH / 2 + 2, RacerGame.HEIGHT - playerCarHeight - 1, ShapeMatrix.PLAYER);
+        super (RacerGame.WIDTH / 2 + 2, RacerGame.HEIGHT - playerCarHeight - 1, ShapeMatrix.PLAYER);
     }
 }
--- JavaRushTasks/8.Games/src/com/javarush/games/racer/RacerGame.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -60,7 +60,7 @@ public class RacerGame extends Game {
         drawScene();
     }
     private void moveAll() {
-        roadMarking.move(player.speed);
+        roadMarking.move (player.speed);
         player.move();
     }
 
@@ -70,5 +70,16 @@ public class RacerGame extends Game {
             player.setDirection(Direction.RIGHT);
         if (key.equals(Key.LEFT))
             player.setDirection(Direction.LEFT);
+    }
+
+    @Override
+    public void onKeyReleased(Key key) {
+        if (key.equals(Key.RIGHT) && player.getDirection().equals(Direction.RIGHT)) {
+            player.setDirection(Direction.NONE);
+        }
+
+        if (key.equals(Key.LEFT) && player.getDirection().equals(Direction.LEFT)) {
+            player.setDirection(Direction.NONE);
+        }
     }
 }
