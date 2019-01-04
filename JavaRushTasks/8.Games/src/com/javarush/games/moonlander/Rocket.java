e4b3f4757821867fde7c0bcc43ee7b93a622b738
tav <tula78@mail.ru>
2019-01-04 15:42:39 +0300

    game moonlander 14-0/23

--- JavaRushTasks/8.Games/src/com/javarush/games/moonlander/Rocket.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/moonlander/Rocket.java
@@ -1,59 +1,75 @@
 package com.javarush.games.moonlander;
 
+import com.javarush.engine.cell.Color;
+
 public class Rocket extends GameObject {
     private double speedY = 0;
     private double speedX = 0;
     private double boost = 0.05;
-    private double slowdown = boost/10;
+    private double slowdown = boost / 10;
 
     public Rocket(double x, double y) {
         super(x, y, ShapeMatrix.ROCKET);
     }
 
-    public void move(boolean isUpPressed, boolean isLeftPressed, boolean isRightPressed){
-        if (isUpPressed)
-            this.speedY-=this.boost;
-        else
-            this.speedY+=this.boost;
-        this.y+=this.speedY;
+    public void move(boolean isUpPressed, boolean isLeftPressed, boolean isRightPressed) {
+        if (isUpPressed) {
+            speedY -= boost;
+        } else {
+            speedY += boost;
+        }
+        y += speedY;
+
         if (isLeftPressed) {
-            this.speedX -= this.boost;
-            this.x+=this.speedX;
+            speedX -= boost;
+            x += speedX;
         } else if (isRightPressed) {
-            this.speedX += this.boost;
-            this.x+=this.speedX;
-        } else if (this.speedX>=-1*this.slowdown&&this.speedX<=this.slowdown) {
-            this.speedX = 0;
-        } else if (this.speedX<-1*this.slowdown) {
-            this.speedX += this.slowdown;
-            this.x+=this.speedX;
-        } else if (this.speedX>this.slowdown) {
-            this.speedX -= this.slowdown;
-            this.x+=this.speedX;
+            speedX += boost;
+            x += speedX;
+        } else if (speedX > slowdown) {
+            speedX -= slowdown;
+        } else if (speedX < -slowdown) {
+            speedX += slowdown;
+        } else {
+            speedX = 0;
         }
-
+        x += speedX;
         checkBorders();
-
     }
-    private void checkBorders() {
-        if (this.x<0) {
-            this.x = 0;
-            this.speedX = 0;
-        }
-        if (this.x+this.width>MoonLanderGame.WIDTH ) {
-            this.x = MoonLanderGame.WIDTH - this.width;
-            this.speedX = 0;
-        }
-        if (this.y<0) {
-            this.y = 0;
-            this.speedY = 0;
-        }
 
+    private void checkBorders() {
+        if (x < 0) {
+            x = 0;
+            speedX = 0;
+        } else if (x + width > MoonLanderGame.WIDTH) {
+            x = MoonLanderGame.WIDTH - width;
+            speedX = 0;
+        }
+        if (y <= 0) {
+            y = 0;
+            speedY = 0;
+        }
     }
 
     public boolean isStopped() {
-        if (this.speedY<10*this.boost)
-            return true;
+        return speedY < 10 * boost;
+    }
+
+    public boolean isCollision(GameObject object) {
+        int transparent = Color.NONE.ordinal();
+
+        for (int matrixX = 0; matrixX < width; matrixX++) {
+            for (int matrixY = 0; matrixY < height; matrixY++) {
+                int objectX = matrixX + (int) x - (int) object.x;
+                int objectY = matrixY + (int) y - (int) object.y;
+
+                if (objectX < 0 || objectX >= object.width || objectY < 0 || objectY >= object.height)
+                    continue;
+
+                if (matrix[matrixY][matrixX] != transparent && object.matrix[objectY][objectX] != transparent)
+                    return true;
+            }
+        }
         return false;
     }
 }
