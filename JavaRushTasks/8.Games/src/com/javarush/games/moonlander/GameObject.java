84943f8dccd77078364724b6bfc8d55db8ce07c7
tav <tula78@mail.ru>
2019-01-04 12:13:32 +0300

    game moonlander 5-1/23

--- JavaRushTasks/8.Games/src/com/javarush/games/moonlander/GameObject.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/moonlander/GameObject.java
@@ -1,13 +1,30 @@
 package com.javarush.games.moonlander;
 
+import com.javarush.engine.cell.*;
+
 public class GameObject {
     public double x;
     public double y;
     public int [][] matrix;
+    public int width;
+    public int height;
 
     public GameObject(double x, double y, int[][] matrix) {
         this.x = x;
         this.y = y;
+        this.width = matrix[0].length;
+        this.height = matrix.length;
         this.matrix = matrix;
+    }
+
+    public void draw(Game game) {
+        if (matrix==null)
+            return;
+        for (int yy=0; yy<matrix.length; yy++) {
+            for (int xx=0; xx<matrix[yy].length; xx++) {
+                game.setCellColor((int)x+xx, (int)y+yy, Color.values()[matrix[yy][xx]]);
+            }
+        }
+
     }
 }
--- JavaRushTasks/8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -5,6 +5,7 @@ import com.javarush.engine.cell.*;
 public class MoonLanderGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    private Rocket rocket;
 
     @Override
     public void initialize() {
@@ -13,14 +14,16 @@ public class MoonLanderGame extends Game {
     }
 
     private void drawScene() {
-        for (int xx=1; xx<=64; xx++) {
-            for (int yy=1; yy<=64; yy++) {
+        for (int xx=0; xx<64; xx++) {
+            for (int yy=0; yy<64; yy++) {
                 setCellColor(xx, yy, Color.BLACK);
             }
         }
+        rocket.draw(this);
     }
 
     private void createGame() {
+        rocket = new Rocket(MoonLanderGame.WIDTH/2, 0);
         drawScene();
     }
 }
